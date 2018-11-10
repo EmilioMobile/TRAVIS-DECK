3d41d8e4d090bc0e49ad12d50edb7fe624693293
Emilio Jose Coronado Lopez <emilio.mobile@gmail.com>
2018-11-10 19:33:33 +0900

    First commit

---
+++ public/js/app.js
@@ -0,0 +1,185 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+(function webpackMissingModule() { throw new Error("Cannot find module \"webpack-dev-server/client?http://localhost:8080\""); }());
+__webpack_require__(1);
+__webpack_require__(2);
+(function webpackMissingModule() { throw new Error("Cannot find module \"./src/client/js/index.js\""); }());
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/*
+	MIT License http://www.opensource.org/licenses/mit-license.php
+	Author Tobias Koppers @sokra
+*/
+/*globals __webpack_hash__ */
+if(false) {
+	var lastHash;
+	var upToDate = function upToDate() {
+		return lastHash.indexOf(__webpack_hash__) >= 0;
+	};
+	var log = require("./log");
+	var check = function check() {
+		module.hot.check().then(function(updatedModules) {
+			if(!updatedModules) {
+				log("warning", "[HMR] Cannot find update. Need to do a full reload!");
+				log("warning", "[HMR] (Probably because of restarting the webpack-dev-server)");
+				return;
+			}
+
+			return module.hot.apply({
+				ignoreUnaccepted: true,
+				ignoreDeclined: true,
+				ignoreErrored: true,
+				onUnaccepted: function(data) {
+					log("warning", "Ignored an update to unaccepted module " + data.chain.join(" -> "));
+				},
+				onDeclined: function(data) {
+					log("warning", "Ignored an update to declined module " + data.chain.join(" -> "));
+				},
+				onErrored: function(data) {
+					log("error", data.error);
+					log("warning", "Ignored an error while updating module " + data.moduleId + " (" + data.type + ")");
+				}
+			}).then(function(renewedModules) {
+				if(!upToDate()) {
+					check();
+				}
+
+				require("./log-apply-result")(updatedModules, renewedModules);
+
+				if(upToDate()) {
+					log("info", "[HMR] App is up to date.");
+				}
+			});
+		}).catch(function(err) {
+			var status = module.hot.status();
+			if(["abort", "fail"].indexOf(status) >= 0) {
+				log("warning", "[HMR] Cannot check for update. Need to do a full reload!");
+				log("warning", "[HMR] " + err.stack || err.message);
+			} else {
+				log("warning", "[HMR] Update check failed: " + err.stack || err.message);
+			}
+		});
+	};
+	var hotEmitter = require("./emitter");
+	hotEmitter.on("webpackHotUpdate", function(currentHash) {
+		lastHash = currentHash;
+		if(!upToDate()) {
+			var status = module.hot.status();
+			if(status === "idle") {
+				log("info", "[HMR] Checking for updates on the server...");
+				check();
+			} else if(["abort", "fail"].indexOf(status) >= 0) {
+				log("warning", "[HMR] Cannot apply update as a previous update " + status + "ed. Need to do a full reload!");
+			}
+		}
+	});
+	log("info", "[HMR] Waiting for update signal from WDS...");
+} else {
+	throw new Error("[HMR] Hot Module Replacement is disabled.");
+}
+
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(3);
+
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* eslint-disable global-require */
+
+
+
+if (true) {
+  module.exports = __webpack_require__(4);
+} else {
+  module.exports = require('./patch.dev');
+}
+
+/***/ }),
+/* 4 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* noop */
+
+
+/***/ })
+/******/ ]);
+//# sourceMappingURL=app.js.map
